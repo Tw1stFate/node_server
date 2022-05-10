d9173db6b16a8e84a9c31715f95cbcccc52a79ad
zzz <flynn.cocoa@gmail.com>
2022-05-10 19:57:13 +0800

    test db

--- module/main.js
+++ module/main.js
@@ -4,6 +4,7 @@ var fs = require("fs");
 const path = require('path');
 var bodyParser = require("koa-bodyparser");
 const { fileURLToPath } = require("url");
+const { resolve } = require("path");
 const sqlite3 = require('sqlite3').verbose();
 
 var app = new Koa();
@@ -24,42 +25,34 @@ router.post("/save", async (ctx, next) => {
     ctx.response.body = 'cookie为空, 请检查格式是否正确!'
     return
   }
+  await saveNewCookies(newCkObj)
+  ctx.response.body = 'update success'
 
-  const ck_file_path = path.join(__dirname, 'cookies')
-  const db_path = '/root/ql/db/database.sqlite'
-  const db = new sqlite3.Database(db_path);
-  db.serialize(() => {
-    db.each("SELECT value FROM Envs", (err, row) => {
-        // console.log(row.id + ": " + row.info);
-        let ckObj = parseCookieStrToObj(row.value)
-        Object.keys(newCkObj).forEach((key) => {
-          ckObj[key] = newCkObj[key]
-        })
-        
-        // convert to formattedCookies
-        let formattedCookies = Object.keys(ckObj).map(key => ckObj[key] + key).join('&')
-        db.run("UPDATE Envs SET value = ? WHERE id = ?", formattedCookies, 1);          ctx.body = '更新成功: ' + JSON.stringify(newCkObj);
-    });
-  });
-  db.close();
-
-  // fs.readFile(ck_file_path, 'utf8', (err, data) => {
-  //   let ckObj = parseCookieStrToObj(data)
-  //   Object.keys(newCkObj).forEach((key) => {
-  //     ckObj[key] = newCkObj[key]
-  //   })
-    
-  //   // convert to formattedCookies
-  //   let formattedCookies = Object.keys(ckObj).map(key => ckObj[key] + key).join('&')
-  //   fs.writeFile(ck_file_path, formattedCookies, (err) => {
-  //     if (err) {
-  //       ctx.body = '更新失败';
-  //       return
-  //     };
-  //     ctx.body = '更新成功: ' + JSON.stringify(newCkObj);
-  //   });
-  // })
 });
+
+async function saveNewCookies(newCkObj) {
+  return new Promise((resolve, reject) => {
+    const db_path = path.join(__dirname, 'tdatabase.sqlite')
+    // const db_path = '/root/ql/db/database.sqlite'
+    const db = new sqlite3.Database(db_path);
+    db.serialize(() => {
+      db.each("SELECT value FROM Envs", (err, row) => {
+          // console.log(row.id + ": " + row.info);
+          let ckObj = parseCookieStrToObj(row.value)
+          Object.keys(newCkObj).forEach((key) => {
+            ckObj[key] = newCkObj[key]
+          })
+          // convert to formattedCookies
+          let formattedCookies = Object.keys(ckObj).map(key => ckObj[key] + key).join('&')
+          db.run("UPDATE Envs SET value = ? WHERE id = ?", formattedCookies, 1)
+          // close db
+          db.close()
+
+          resolve()
+      });
+    });
+  })
+}
 
 function parseCookieStrToObj(cookiesString) {
   let ckObj = {}
